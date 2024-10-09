0677d6120d2cbcfb96674e351d028f05bc4c2963
h1ddenscript <h1ddenscripto@protonmail.com>
2024-10-09 17:33:06 +0100

    new basic embed new messagecollection new shout

--- src/commands/shout.ts
+++ src/commands/shout.ts
@@ -1,17 +1,15 @@
 import type { ButtonInteraction, ColorResolvable, CommandInteraction, GuildBasedChannel, GuildTextBasedChannel, MessageActionRowComponentBuilder, TextBasedChannel, TextChannel } from 'discord.js'
-import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, EmbedBuilder } from 'discord.js'
+import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, Embed, EmbedBuilder } from 'discord.js'
 import { Bot, ButtonComponent, Discord, Guild, Slash, SlashOption } from 'discordx'
 
-import { textChannelQuestion, stringQuestion, imageQuestion } from '../util/messageCollection.js'
+import { textChannelQuestion, stringQuestion, attachmentQuestion, optionQuestion } from '../util/messageCollection.js'
+import { styleText } from 'util'
+import { basicEmbed } from '../util/basicEmbed.js'
 
 const timeEachQuestion = 216000_000 // 1 hour
 
 @Discord()
-@Guild('1102691674104275097', '1122953128703168532')
 export class AnnounceCommand {
-	private embed: EmbedBuilder | undefined
-	private channel: GuildTextBasedChannel | undefined
-
 	@Slash({
 		name: 'shout',
 		description: 'Announce a message to a channel.',
@@ -25,103 +23,122 @@ export class AnnounceCommand {
 			required: false,
 			type: ApplicationCommandOptionType.String,
 		})
-		color: string | undefined,
+		color: ColorResolvable | undefined,
 		interaction: CommandInteraction,
 	) {
+		if (!interaction.isRepliable()) return;
+
 		try {
-			function isValidHexColor(hex: string): string | null {
-				const hexColorPattern = /^[0-9A-Fa-f]{6}$/
+			const embed = new EmbedBuilder()
 
-				const colorCode = String(hex).replace('/^#/', '')
-
-				if (hexColorPattern.test(colorCode)) {
-					const finalColorCode = hex.startsWith('#') ? hex : `#${colorCode}`
-					return finalColorCode
+			if (color) {
+				if (color.toString()[0] !== '#') {
+					embed.setColor(`#${color.toString()}`)
 				} else {
-					return null
+					embed.setColor(color)
 				}
-			}
-
-			this.embed = new EmbedBuilder()
-
-			if (color && isValidHexColor(color)) {
-				this.embed.setColor(isValidHexColor(color) as ColorResolvable)
 			} else {
-				this.embed.setColor('#2b2d31')
+				embed.setColor('#2b2d31')
 			}
 
-			this.channel = await textChannelQuestion(interaction, 'What channel would you like to send this message to?', {}, timeEachQuestion, 'You must mention/tag a channel!')
-
-			let title = await stringQuestion(interaction, 'What is the title of the announcement?', true, timeEachQuestion)
-
-			let description = await stringQuestion(interaction, 'What is the description of the announcement?', false, timeEachQuestion)
-
-			let image = await imageQuestion(interaction, 'What is the image for the announcement?', true, timeEachQuestion)
-
-			let footer = await stringQuestion(interaction, 'What is the footer of the announcement?', true, timeEachQuestion)
-
-			if (title) {
-				this.embed.setTitle(title)
-			}
-
-			if (description) {
-				this.embed.setDescription(description)
-			}
-
-			if (image) {
-				this.embed.setImage(image)
-			}
-
-			if (footer) {
-				if (footer.toLowerCase() === 'me') {
-					this.embed.setFooter({
-						text: `${interaction.user.username}`,
-						iconURL: interaction.user.displayAvatarURL(),
-					})
-				} else {
-					this.embed.setFooter({ text: footer })
-				}
-			}
-
-			interaction.followUp({
-				content: `Heres a preview of your announcement:`,
-				embeds: [this.embed],
-				components: [new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(new ButtonBuilder().setLabel('@everyone').setStyle(ButtonStyle.Secondary).setCustomId('everyone'), new ButtonBuilder().setLabel('@here').setStyle(ButtonStyle.Secondary).setCustomId('here'), new ButtonBuilder().setLabel('None').setStyle(ButtonStyle.Secondary).setCustomId('none'), new ButtonBuilder().setLabel('Cancel').setStyle(ButtonStyle.Danger).setCustomId('cancel'))],
+			const channel = await textChannelQuestion({
+				interaction,
+				question: 'What channel would you like to send this message to?'
 			})
-		} catch (error) {
-			// Unhandled
-		}
-	}
 
-	@ButtonComponent({ id: 'everyone' })
-	async everyone_handler(interaction: ButtonInteraction): Promise<void> {
-		interaction.message.edit({ content: `Announced to ${this.channel?.url}!`, components: [] })
-		if (this.embed) {
-			await this.channel?.send({ content: `@everyone`, embeds: [this.embed] })
-			return
-		}
-	}
+			const title = await stringQuestion({
+				interaction,
+				question: 'What is the title of the announcement?',
+				skippable: true
+			})
 
-	@ButtonComponent({ id: 'here' })
-	async here_handler(interaction: ButtonInteraction): Promise<void> {
-		interaction.message.edit({ content: `Announced to ${this.channel?.url}!`, components: [] })
-		if (this.embed) {
-			await this.channel?.send({ content: `@here`, embeds: [this.embed] })
-			return
-		}
-	}
+			if (title.response) embed.setTitle(title.response)
 
-	@ButtonComponent({ id: 'none' })
-	async none_handler(interaction: ButtonInteraction): Promise<void> {
-		interaction.message.edit({ content: `Announced to ${this.channel?.url}!`, components: [] })
-		if (this.embed) {
-			await this.channel?.send({ embeds: [this.embed] })
-			return
-		}
-	}
+			const description = await stringQuestion({
+				interaction,
+				question: 'What is the description of the announcement?',
+			})
 
-	@ButtonComponent({ id: 'cancel' })
-	async handler(interaction: ButtonInteraction): Promise<void> {
-		await interaction.message.edit({ content: `Cancelled`, components: [], embeds: [] })
+			embed.setDescription(description.response)
+
+			const image = await attachmentQuestion({
+				interaction,
+				question: 'What is the image for the announcement?',
+				skippable: true
+			})
+
+			if (image.response) embed.setImage(image.response?.url)
+
+			const footer = await stringQuestion({
+				interaction,
+				question: 'What is the footer of the announcement?',
+				skippable: true
+			})
+
+			if (footer.response === 'me' && interaction.user.avatar) {
+				embed.setFooter({ text: interaction.user.username, iconURL: interaction.user.avatarURL() || undefined })
+			} else if (footer.response) {
+				embed.setFooter({ text: footer.response })
+			}
+
+			optionQuestion({
+				interaction,
+				question: 'Heres a preview of your announcement:',
+				extra_embeds: [embed]
+			}, [
+				{
+					id: 'everyone',
+					option: '@everyone',
+					style: ButtonStyle.Secondary
+				},
+				{
+					id: 'here',
+					option: '@here',
+					style: ButtonStyle.Secondary
+				},
+				{
+					id: 'none',
+					option: 'None',
+					style: ButtonStyle.Secondary
+				},
+				{
+					id: 'cancel',
+					option: 'Cancel',
+					style: ButtonStyle.Danger
+				}
+			]).then((value) => {
+				if (!value.response || value.response === 'cancel') {
+					value.originalMessage.edit({
+						content: 'Cancelled.',
+						embeds: [embed],
+						components: []
+					})
+					return;
+				}
+
+				let content: string = ''
+
+				channel.response?.send({
+					content: (value.response !== 'none' && `@${value.response}`) || undefined,
+					embeds: [embed]
+				})
+
+				value.originalMessage.edit({
+					content: 'Sent!',
+					embeds: [embed],
+					components: []
+				})
+
+				return;
+
+
+			})
+		} catch(err) {
+			interaction.followUp({
+				embeds: [basicEmbed('ALERT', "Something went wrong! Please try again.")]
+			})
+
+			console.warn(err);
+		}
 	}
 }
